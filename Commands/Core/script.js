09bee410e15bb0b605300684cf036543bd433fe8
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2023-03-04 00:07:59 +0530

    🕯️✨ʀᴇᴅᴢᴇᴏꭗ✨🕯️

--- Commands/Core/script.js
+++ Commands/Core/script.js
@@ -9,9 +9,9 @@ module.exports = {
     category: "Core",
     start: async(Miku, m,{pushName,prefix}) => {
         let picURL = fs.readFileSync('./Page/BG.jpg')
-        let repoInfo = await axios.get('https://api.github.com/repos/FantoX001/Atlas-MD')
+        let repoInfo = await axios.get('*🎐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ꜱᴄᴏᴛᴄʜ 🎐*')
         let repo = repoInfo.data
-        let txt = `      🧣 *${botName}'s Script* 🧣\n\n*🎀 Total Forks:* ${repo.forks_count}\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${repo.license.name}\n*📁 Repo Size:* ${(repo.size/1024).toFixed(2)} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${repo.html_url}\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless harkwork by *Team ATLAS*. ❞\n\n*©️ Team ATLAS- 2023*`
+        let txt = `      🧣 *${botName}'s Script* 🧣\n\n*🎀 Total Forks:* ${repo.forks_count}\n*✧ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${repo.license.name}\n*📁 Repo Size:* ${(repo.size/1024).toFixed(2)} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${repo.html_url}\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless harkwork by *Team ATLAS*. ❞\n\n*©️ Team ATLAS- 2023*`
         await Miku.sendMessage(m.from,{image:picURL, caption:txt},{quoted:m});
     }
 }
